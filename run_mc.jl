60c361e505296bb88f18140b27748e4e69603071
W. Joe Meese <meese022@umn.edu>
2022-05-09 18:25:44 -0700

    Need to determine how to include parameters in a nice way in the data directory

--- run_mc.jl
+++ run_mc.jl
@@ -2,19 +2,11 @@ using Pkg
 Pkg.activate(@__DIR__)
 
 include("src/ParametersJSON.jl")
+include("src/File_Handling/FileHandling.jl")
 include("src/Lattices/CubicLattice2D.jl")
 include("src/Ashkin_Teller/AT_Hamiltonian.jl")
 include("src/Ashkin_Teller/AT_State.jl")
 include("src/Monte_Carlo_Core/MonteCarloCore.jl")
-
-function run_default_metropolis()
-    latt = CubicLattice2D()
-    ham = AT_Hamiltonian{Float64}(latt)
-    mc_params = MetropolisParameters{Float64}()
-    mc_model = Model(latt, ham)
-    thermalize!(mc_model, mc_params, metropolis_sweep!)
-    sweep_and_measure!(mc_model, mc_params, metropolis_sweep!, nothing)
-end
 
 using BenchmarkTools
 latt = CubicLattice2D()
--- src/File_Handling/FileHandling.jl
+++ src/File_Handling/FileHandling.jl
@@ -3,7 +3,11 @@ using Dates
 const default_mc_root = abspath(joinpath(@__DIR__,"..","..","NMR_Simulation_Data"))
 const mc_date_format = "u-dd-yyyy"
 
+eligible_models() = ["Ashkin-Teller", "RBFM"]
+
 function build_data_directory(; model_name, data_root = default_mc_root)
+    model_name ∈ eligible_models() ? nothing : error("\nModel name $(model_name) not an eligible type.\n")
     data_path = joinpath(data_root, model_name, Dates.format(Dates.now(), mc_date_format))
     return mkpath(data_path)
 end
+
