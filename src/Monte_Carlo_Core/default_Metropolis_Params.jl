27bbdc2db1c523bc9a46574fecf6b75eab7494da
W. Joe Meese <meese022@umn.edu>
2022-05-11 18:14:38 -0700

    Increase default parameters

--- src/Monte_Carlo_Core/default_Metropolis_Params.jl
+++ src/Monte_Carlo_Core/default_Metropolis_Params.jl
@@ -1,6 +1,6 @@
 {
                     "β": 0.441,
          "therm_sweeps": 262144,
-       "measure_sweeps": 4096,
-   "total_measurements": 4096
+       "measure_sweeps": 65536,
+   "total_measurements": 65536
 }
