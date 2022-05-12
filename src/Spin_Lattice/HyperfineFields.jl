1f90770fca36f8fa61621a445fcee03481b9c1e0
W. Joe Meese <meese022@umn.edu>
2022-05-11 17:30:16 -0700

    Add _Holy Traits_ for the `mag_vectors` to use different projections

--- src/Spin_Lattice/HyperfineFields.jl
+++ src/Spin_Lattice/HyperfineFields.jl
@@ -36,58 +36,78 @@ const hyperfine_Amats = @SVector [ hyp_A1, hyp_A2, hyp_A3, hyp_A4 ]
 @enum As_atoms As_plus=1 As_minus As_enum_end
 const NUM_As_ATOMS = Int(As_enum_end) - Int(As_plus)
 
-mag_vector(state, site, color) = @SVector [0,0, state[site, color]]
+struct OutofPlane end
+struct SpinOrbitCoupling end
+const mag_vector_types = @SVector [OutofPlane, SpinOrbitCoupling]
 
-function hyperfine_plus( state, latt::CubicLattice2D, site )
-    hyp1 = hyperfine_Amats[4] * mag_vector(state, site, AT_tau)
-    hyp2 = hyperfine_Amats[1] * mag_vector(state, site_index(latt, site, (0, 1)), AT_tau)
-    hyp3 = hyperfine_Amats[3] * mag_vector(state, site, AT_sigma)
-    hyp4 = hyperfine_Amats[2] * mag_vector(state, site_index(latt, site, (1, 0)), AT_sigma)
+mag_vector(ty::Type{T}...) where {T} = error("\nNo method defined for mag_vectors with the $(ty) trait.")
+mag_vector(::Type{OutofPlane}, state, site, color) = @SVector [0,0, state[site, color]]
+function mag_vector(::Type{SpinOrbitCoupling}, state, site, color)
+    if site_Baxter(state, site) == one(eltype(state))
+        return @SVector [state[site, color], 0, 0]
+    end
+    return @SVector [0, state[site, color], 0]
+end
+
+function hyperfine_plus(ty, state, latt::CubicLattice2D, site )
+    hyp1 = hyperfine_Amats[4] * mag_vector(ty, state, site, AT_tau)
+    hyp2 = hyperfine_Amats[1] * mag_vector(ty, state, site_index(latt, site, (0, 1)), AT_tau)
+    hyp3 = hyperfine_Amats[3] * mag_vector(ty, state, site, AT_sigma)
+    hyp4 = hyperfine_Amats[2] * mag_vector(ty, state, site_index(latt, site, (1, 0)), AT_sigma)
     return hyp1 - hyp2 + hyp3 - hyp4
 end
 
-function hyperfine_minus( state, latt::CubicLattice2D, site )
-    hyp1 = hyperfine_Amats[1] * mag_vector(state, site, AT_sigma)
-    hyp2 = hyperfine_Amats[4] * mag_vector(state, site_index(latt, site, (0, -1)), AT_sigma)
-    hyp3 = hyperfine_Amats[2] * mag_vector(state, site, AT_tau)
-    hyp4 = hyperfine_Amats[3] * mag_vector(state, site_index(latt, site, (-1, 0)), AT_tau)
+function hyperfine_minus(ty, state, latt::CubicLattice2D, site )
+    hyp1 = hyperfine_Amats[1] * mag_vector(ty, state, site, AT_sigma)
+    hyp2 = hyperfine_Amats[4] * mag_vector(ty, state, site_index(latt, site, (0, -1)), AT_sigma)
+    hyp3 = hyperfine_Amats[2] * mag_vector(ty, state, site, AT_tau)
+    hyp4 = hyperfine_Amats[3] * mag_vector(ty, state, site_index(latt, site, (-1, 0)), AT_tau)
     return hyp1 - hyp2 + hyp3 - hyp4
 end
 
-function hyperfine_fields( state, latt::CubicLattice2D, site )
-    return @SVector [ hyperfine_plus(state, latt, site), hyperfine_minus(state, latt, site) ]
+function hyperfine_fields(ty, state, latt::CubicLattice2D, site )
+    return @SVector [ hyperfine_plus(ty, state, latt, site), hyperfine_minus(ty, state, latt, site) ]
 end
 
-function inst_hyperfine_fluctuations( state, latt::CubicLattice2D, site )
-    fields = hyperfine_fields(state, latt, site)
+function inst_hyperfine_fluctuations(ty, state, latt::CubicLattice2D, site )
+    fields = hyperfine_fields(ty, state, latt, site)
     return @SVector [fields[1][1] * fields[1][1] + fields[1][3] * fields[1][3],
                      fields[2][1] * fields[2][1] + fields[2][3] * fields[2][3] ]
 end
 
-function populate_hyperfine_fluctuations!(flucts, state, latt::CubicLattice2D) where {T}
+function populate_hyperfine_fluctuations!(ty, flucts, state, latt::CubicLattice2D) where {T}
     if length(flucts) != num_sites_CL2D(latt.params) * NUM_As_ATOMS
         error("\nFluctuations state is of incorrect length: $(length(flucts)) != $(num_sites_CL2D(latt.params) * NUM_As_ATOMS)")
     end
 
     @inbounds for site ∈ eachindex(1:num_sites_CL2D(latt.params))
         range = @SVector [NUM_As_ATOMS * (site - 1) + 1, NUM_As_ATOMS * (site - 1) + NUM_As_ATOMS]
-        flucts[ range[1]:range[2] ] .= inst_hyperfine_fluctuations(state, latt, site)
+        flucts[ range[1]:range[2] ] .= inst_hyperfine_fluctuations(ty, state, latt, site)
     end
     return nothing
 end
 
-function populate_all_hyperfine_fluctuations!(all_flucts, all_states, latt::CubicLattice2D)
+function populate_all_hyperfine_fluctuations!(ty, all_flucts, all_states, latt::CubicLattice2D)
     # all_flucts and all_states have reversed sizes
-    # for speed on all_flucts analysis
+    # for speed on subsequent all_flucts analyses
     if size(all_flucts) != reverse(size(all_states))
         error("\nFluctuations state and MC state do not match in size: $(size(all_flucts)) != reverse($(size(all_states)))")
     end 
     @inbounds for state_idx ∈ eachindex(1:size(all_flucts)[1])
         flucts = @view all_flucts[state_idx, :]
         state =  @view all_states[:, state_idx]
-        populate_hyperfine_fluctuations!( flucts, state, latt)
+        populate_hyperfine_fluctuations!(ty, flucts, state, latt)
     end
     return nothing
+end
+
+function analyze_fluctuations!( all_flucts, all_states, latt::CubicLattice2D; analysis = mean )
+    fluct_dists = zeros( size(all_flucts)[2], length(mag_vector_types) )
+    @inbounds for (tydx, ty) ∈ enumerate(mag_vector_types)
+        populate_all_hyperfine_fluctuations!(ty, all_flucts, all_states, latt)
+        fluct_dists[:, tydx] .= [ analysis(all_flucts[:, idx]) for idx ∈ 1:size(all_flucts)[2] ]
+    end
+    return fluct_dists
 end
 
 
