080488131e956b65decd20421fbe43aad8b29b2b
W. Joe Meese <meese022@umn.edu>
2022-05-30 13:32:54 -0500

    The low-temperature average is still not right, but I'm quite confident now it's a problem of ergodicity

--- src/Monte_Carlo_Core/default_Metropolis_Params.jl
+++ src/Monte_Carlo_Core/default_Metropolis_Params.jl
@@ -1,6 +1,6 @@
 {
               "βvalues": [0.0000441, 0.441, 0.882],
          "therm_sweeps": 262144,
-       "measure_sweeps": 65536,
+       "measure_sweeps": 262144,
    "total_measurements": 65536
 }
