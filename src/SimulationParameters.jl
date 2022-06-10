f71ec2282c255b3b0d6e758e297e3e49076bb540
W. Joe Meese <meese022@umn.edu>
2022-06-10 15:40:48 -0500

    Bring `SimulationParameters` into the `Parameters2JSON.jl` fold

--- src/SimulationParameters.jl
+++ src/SimulationParameters.jl
@@ -1,10 +1,11 @@
-abstract type SimulationParameters end
-num_exports(simparams::SimulationParameters) = error("\nNo method defined for $(typeof(sim_params)) types.")
+using Parameters2JSON
 
-struct AT_2DCL_Metro_Params{T <: AbstractFloat} <: SimulationParameters
+abstract type SimulationParameters end
+num_exports(simparams::SimulationParameters) = error("\nNo method defined for $(typeof(simparams)) types.")
+
+@jsonable struct AT_2DCL_Metro_Params{T <: AbstractFloat} <: SimulationParameters
     latt_params::CubicLattice2DParams
     ham_params::AT_Parameters{T}
     mc_params::MetropolisParameters{T}
 end
-StructTypes.StructType(::Type{AT_2DCL_Metro_Params{T}}) where {T} = StructTypes.Struct()
 num_exports(sim_params::AT_2DCL_Metro_Params) = sim_params.mc_params.total_measurements
