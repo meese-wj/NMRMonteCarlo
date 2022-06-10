b8a1f2662974a76e3b7f9646af1dc9d36694d7fc
W. Joe Meese <meese022@umn.edu>
2022-06-10 13:34:44 -0500

    Promote quick Rx plotting functionality to all `Binner`s

--- src/Monte_Carlo_Core/BinningAnalysis/MCBinningAnalysis.jl
+++ src/Monte_Carlo_Core/BinningAnalysis/MCBinningAnalysis.jl
@@ -55,7 +55,7 @@ num_bins( record::AbstractVector, bin_level ) = num_bins(length(record), bin_lev
 num_bins( bins::Binner, bin_level ) = num_bins(bins.time_record, bin_level)
 num_bins( bins::Binner ) = [ num_bins(bins.time_record, bin_level) for bin_level ∈ 1:bin_depth(bins) ]
 
-function Binner( record::AbstractVector; analyze = false )
+function Binner( record::AbstractVector; analyze = true )
     num_t = length(record)
     max_bin_level = bin_depth(num_t)
     bins = Binner( mean(record), var_of_mean(record), record,
@@ -130,6 +130,9 @@ function generate_poisson( τ, signal_length )
 
     signal::Vector{Float64} = []
     stepsize = floor(Int, -τ * log( 1 - rand() ))
+    while stepsize < 1
+        stepsize = floor(Int, -τ * log( 1 - rand() ))
+    end
     append!(signal, ones(stepsize))
     while length(signal) < Int(signal_length)
         stepsize = floor(Int, -τ * log( 1 - rand() ))
@@ -146,20 +149,32 @@ reldiff(a, b) = abs(a - b) / b
 reldiff(a::AbstractVector) = broadcast( (a,b) -> reldiff(a,b), a[2:end], a[1:end-1] )
 # end
 
+function bin_plot!( plt1, plt2, bins::Binner )
+    plot!(plt1, bin_size(bins), Rx(bins); 
+          xscale = :log10, yscale = :identity, 
+          xlabel = L"Bin Size $2^\ell$", ylabel = L"$R_X$", 
+          markershape = :circle, leg = false )
+
+    plot!(plt2, bin_size(bins)[2:end], reldiff(Rx(bins)); 
+          xscale = :log10, yscale = :identity, 
+          xlabel = L"Bin Size $2^\ell$", ylabel = L"$\Delta R_X / R_X$", 
+          markershape = :circle, label = "\$\\ell_{\\textrm{max}} = $(bin_depth(bins))\$" )
+    return nothing
+end
+
+function bin_plot( record; plot_title = "" )
+    plt1 = plot()
+    plt2 = plot()
+    bin_plot!(plt1, plt2, Binner(record))
+    plot(plt1, plt2; layout = (1,2), link = :x, figsize = (800, 450), plot_title = plot_title)
+end
+
 function poisson_plots( τ, minpow2, maxpow2, incr = 2 )
     plt1 = plot()
     plt2 = plot()
     for pow2 ∈ minpow2:incr:maxpow2
         pt_binner = Binner( generate_poisson(τ, 2^pow2), analyze = true )
-        plot!(plt1, bin_size(pt_binner), Rx(pt_binner); 
-              xscale = :log10, yscale = :identity, 
-              xlabel = L"Bin Size $2^\ell$", ylabel = L"$R_X$", 
-              markershape = :circle, leg = false )
-
-        plot!(plt2, bin_size(pt_binner)[2:end], reldiff(Rx(pt_binner)); 
-              xscale = :log10, yscale = :identity, 
-              xlabel = L"Bin Size $2^\ell$", ylabel = L"$\Delta R_X / R_X$", 
-              markershape = :circle, label = "\$\\ell_{\\textrm{max}} = $(bin_depth(pt_binner))\$" )
+        bin_plot!(plt1, plt2, pt_binner)
     end
     plot(plt1, plt2; layout = (1,2), link = :x, figsize = (800, 450))
 end
