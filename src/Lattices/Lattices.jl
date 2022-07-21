842880d4c588aa4f06da59a9e26a15dc8e30d61e
W. Joe Meese <meese022@umn.edu>
2022-07-21 14:00:43 -0500

    Add some docs for `Lattices.jl`

--- src/Lattices/Lattices.jl
+++ src/Lattices/Lattices.jl
@@ -1,3 +1,15 @@
+"""
+    module Lattices
+
+Main entry point for any simulated model to access an underlying geometrical structure.
+Currently comprised of the following submodules:
+
+- [`AbstractLattices`](@ref):
+    - Interface module for all types of lattices
+- [`CubicLattices`](@ref): 
+    - Defines the [`AbstractCubicLattice`](@ref) subtype
+    - Implements the [`CubicLattice2D`](@ref) `<: AbstractCubicLattice` and the [`CubicLattice2DParams`](@ref)  
+"""
 module Lattices
 
 using Reexport
