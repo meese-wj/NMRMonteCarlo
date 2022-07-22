d62f0dded084ff19176e7571d345640a77e5036d
W. Joe Meese <meese022@umn.edu>
2022-07-22 17:40:33 -0500

    Add some documentation for `NMRMonteCarlo.jl`

--- src/NMRMonteCarlo.jl
+++ src/NMRMonteCarlo.jl
@@ -1,3 +1,12 @@
+"""
+    module NMRMonteCarlo
+
+Main `src` entry point for the simulation of nuclear magnetic resonance
+(NMR) signals in the _Fast Dynamics Limit_ using Monte Carlo Methods.
+This limit holds whenever the typical relaxation time is much smaller
+than the period of oscillation associated with 
+[Larmor precession](https://en.wikipedia.org/wiki/Larmor_precession?oldformat=true).
+"""
 module NMRMonteCarlo
 
 import Reexport: @reexport
