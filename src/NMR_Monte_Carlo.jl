96c4810494a17ee9ac49056f47b870ff2a5bd07a
W. Joe Meese <meese022@umn.edu>
2022-07-22 16:57:16 -0500

    Get rid of the empty src module

--- src/NMR_Monte_Carlo.jl
+++ src/NMR_Monte_Carlo.jl
@@ -1,5 +1,12 @@
 module NMR_Monte_Carlo
 
-# intentionally empty for testing...
-    
+import Reexport: @reexport
+include( joinpath( "Lattices", "Lattices.jl" ) )
+include( joinpath( "Hamiltonians", "Hamiltonians.jl" ) )
+include( joinpath( "MonteCarloMethods", "MonteCarloMethods.jl" ) )
+
+@reexport using .Lattices
+@reexport using .Hamiltonians
+@reexport using .MonteCarloMethods
+
 end # NMR_Monte_Carlo
