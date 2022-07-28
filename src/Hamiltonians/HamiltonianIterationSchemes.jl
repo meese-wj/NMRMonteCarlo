5af698977a3fd1cf92f48236ccf490311de9b0a5
W. Joe Meese <meese022@umn.edu>
2022-07-28 16:59:24 -0500

    Add docs for `HamiltonianIterator`

--- src/Hamiltonians/HamiltonianIterationSchemes.jl
+++ src/Hamiltonians/HamiltonianIterationSchemes.jl
@@ -39,6 +39,47 @@ Create an iterator trait for various `subtype`s of [`HamiltonianIterationScheme`
 """
 IterationScheme(::Type{T}) where {T <: HamiltonianIterationScheme} = T()
 
-iterate(iter::Tuple{<: HamiltonianIterationScheme, <: Any}, args...) = iterate(iter..., args...)
-iterate(a::Any, b::Type{<: HamiltonianIterationScheme}, args...) = iterate((b, a), args...)
-enumerate(a::Any, b::Type{<: HamiltonianIterationScheme}) = enumerate((b, a))
+"""
+    HamiltonianIterator{H, S <: HamiltonianIterationScheme}
+
+A wrapper iterator around a given Hamiltonian of type `H`. 
+
+# Fields 
+
+- `iter`: a pointer to the reference Hamiltonian
+
+# Constructor
+
+    HamiltonianIterator(ham::H, ::Type{S})
+
+Creates a new `HamiltonianIterator` with respect to the Hamiltonian 
+`ham` with a given `S <:`[`HamiltonianIterationScheme`](@ref).
+"""
+struct HamiltonianIterator{H, S <: HamiltonianIterationScheme} 
+    iter::H
+    HamiltonianIterator(ham::H, ::Type{S}) where {H, S} = new{H, S}(ham)
+end
+"""
+    IterationScheme(::HamiltonianIterator{H, S}) → S 
+
+Return the [`HamiltonianIterationScheme`](@ref) from a [`HamiltonianIterator`](@ref).
+"""
+IterationScheme(::HamiltonianIterator{H, S}) where {H, S} = S()
+"""
+    HamiltonianType(::HamiltonianIterator{H, S}) → H 
+
+Return the Hamiltonian `Type` from a [`HamiltonianIterator`](@ref).
+"""
+HamiltonianType(::HamiltonianIterator{H, S}) where {H, S} = H
+"""
+    iterate(scheme::Type{<: HamiltonianIterationScheme}, ham, args...)
+
+Wrapper for `iterate(HamiltonianIterator(ham, scheme), args...)`.
+"""
+iterate(scheme::Type{<: HamiltonianIterationScheme}, ham, args...) = iterate( HamiltonianIterator(ham, scheme), args...)
+"""
+    enumerate(scheme::Type{<: HamiltonianIterationScheme}, ham)
+
+Wrapper for `enumerate(HamiltonianIterator(ham, scheme))`.
+"""
+enumerate(scheme::Type{<: HamiltonianIterationScheme}, ham) = enumerate(HamiltonianIterator(ham, scheme))
