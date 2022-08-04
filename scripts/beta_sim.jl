6b19c1394552329a0f3f3a9884ddc801dc260c0f
W. Joe Meese <meese022@umn.edu>
2022-08-04 14:14:42 -0500

    Tested the runtime analysis

--- scripts/beta_sim.jl
+++ scripts/beta_sim.jl
@@ -7,7 +7,7 @@ function create_test_suite(temperature = 2.5, Lx=16, Ly=Lx)
     latt = CubicLattice2D(Lx, Ly)
     atparams = AshkinTellerParameters(1., 0.)
     ham = AshkinTellerHamiltonian(latt, atparams)
-    metroparams = MetropolisParameters{Float64}([1/temperature], 2^12, 2^12, 2^12)
+    metroparams = MetropolisParameters{Float64}([1/temperature], 2^16, 2^16, 2^16)
 
     model = CleanAshkinTellerModel( latt.params.Lx, latt.params.Ly, atparams.Jex, atparams.Kex, metroparams.total_measurements )
     return metroparams, model
