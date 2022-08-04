97d1dd5f046fe9963c9c485dd522bcf7cbaf9259
W. Joe Meese <meese022@umn.edu>
2022-08-04 14:28:03 -0500

    Memory allocations from the simulation occur in the binning_analysis

--- scripts/beta_sim.jl
+++ scripts/beta_sim.jl
@@ -3,7 +3,7 @@ using DrWatson
 @quickactivate :NMRMonteCarlo
 using MonteCarloMeasurementUncertainty
 
-function create_test_suite(temperature = 2.5, Lx=16, Ly=Lx)
+function create_test_suite(temperature = 2.5, Lx=32, Ly=Lx)
     latt = CubicLattice2D(Lx, Ly)
     atparams = AshkinTellerParameters(1., 0.)
     ham = AshkinTellerHamiltonian(latt, atparams)
@@ -17,8 +17,8 @@ function simulate!( model, metroparams )
     @info "Start of simulation."
 
     beta = metroparams.βvalues[begin]
-    thermalize!(model, beta, metroparams, metropolis_sweep!)
-    sweep_and_measure!(model, beta, metroparams, metropolis_sweep!)
+    @show @allocated thermalize!(model, beta, metroparams, metropolis_sweep!)
+    @show @allocated sweep_and_measure!(model, beta, metroparams, metropolis_sweep!)
     
     for obs ∈ Observables(model)
         @show name(obs)
@@ -30,6 +30,6 @@ end
 
 metroparams, model = create_test_suite()
 timer = @timed simulate!(model, metroparams)
-println()
-@info "$(round( (metroparams.therm_sweeps + metroparams.measure_sweeps) * num_DoF(Hamiltonian(model)) / timer.time, sigdigits = 4) ) updates/second"
+@info "Simulation time: $(round(timer.time; sigdigits=4)) seconds"
+@info "$(round( (metroparams.therm_sweeps + metroparams.measure_sweeps) * num_DoF(Hamiltonian(model)) / timer.time; sigdigits = 4) ) updates/second"
 @info "$( timer.bytes / 1000 ) KiB allocated"
