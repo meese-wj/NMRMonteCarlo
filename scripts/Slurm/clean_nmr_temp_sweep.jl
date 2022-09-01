5f7e0761b5236a55ad81347837041a45403cad16
W. Joe Meese <meese022@umn.edu>
2022-09-01 15:50:25 -0500

    Modify Slurm script for job submission

--- scripts/Slurm/clean_nmr_temp_sweep.jl
+++ scripts/Slurm/clean_nmr_temp_sweep.jl
@@ -1,12 +1,12 @@
 #!/usr/bin/bash -l
-#SBATCH --time=1:00:00
+#SBATCH --time=5:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=5
 #SBATCH --mem=10g
 #SBATCH --mail-type=all
 #SBATCH --mail-user=meese022@umn.edu
 #SBATCH --array=1-50
-#SBATCH --job-name=nmr-L-64_%A_%a.out
+#SBATCH --job-name=nmr-L-32_%A_%a.out
 #SBATCH -o %x-%j.out
 #=
     pwd
@@ -17,7 +17,8 @@
     exit
 =#
 
-using DrWatson; @quickactivate :NMRMonteCarlo
+using DrWatson
+@quickactivate :NMRMonteCarlo
 
 const slurm_arr_length::Int = parse(Int, ENV["SLURM_ARRAY_TASK_COUNT"])
 
@@ -25,22 +26,23 @@ beta_vals = LinRange( 1/3, 1/2, slurm_arr_length )
 
 const my_index = parse(Int, ENV["SLURM_ARRAY_TASK_ID"])
 const my_beta::Float64 = beta_vals[my_index] 
+const nmr_type::Type = Out_of_Plane
 
 @info "Setting up a small run first."
-sim = CleanNMRATMSimulation(; Lx = 4, βvalue= my_beta )
+sim = CleanNMRATMSimulation( Lx = 4, βvalue = my_beta, nmr_spin_type = nmr_type)
 
 println(sim)
 @timev simulate!(sim)
 @info "End of the small simulation."
 
 @info "Starting real run."
-sim = CleanNMRATMSimulation(; Lx = 64, βvalue= my_beta, Ntherm =2^18)
+sim = CleanNMRATMSimulation(Lx = 64, βvalue= my_beta, Ntherm = 2^20, Nmeas = 2^18, nmr_spin_type = nmr_type)
 @show sim
 
 @timev simulate!(sim)
 @info "End of real run."
 
-datapath = savename("clean_temp_sweep", SimulationParameters(sim), "jld2")
+datapath = savename("clean_temp_sweep_$(nmr_type)", SimulationParameters(sim), "jld2")
 @info "Find the data at: $( datadir(datapath) )"
 
 using JLD2
